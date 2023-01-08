22df1f8a2e18ce965f9b993e56d86bab391ad973
edilk <ed2803k@gmail.com>
2023-01-08 22:09:58 +0600

    OOP tasks done

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -2,4 +2,20 @@ package com.epam.OOP;
 
 public class Animal {
 
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String paw = numberOfPaws > 1 ? "paws" : "paw";
+        String hasOrNot = hasFur ? "a" : "no";
+        return "This animal is mostly " + color
+                + ". It has " + numberOfPaws + paw + " and " + hasOrNot + " fur.";
+    }
 }

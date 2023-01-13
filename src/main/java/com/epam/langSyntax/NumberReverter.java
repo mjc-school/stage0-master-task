233f951798c5396907ab2fab781f9913d4525b31
edilk <ed2803k@gmail.com>
2023-01-13 13:30:34 +0600

    Lang Syntax tasks are done

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,12 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
-    }
+        String str = String.valueOf(number);
+        StringBuilder reverted = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverted.append(str.charAt(i));
+        }
 
+        System.out.println(reverted.toString());
+    }
 }

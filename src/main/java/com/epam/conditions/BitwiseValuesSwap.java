b3a42f3159891f112b56d3bc9aff2d54f46e8484
edilk <ed2803k@gmail.com>
2023-01-13 21:13:41 +0600

    Conditions tasks are done

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -3,6 +3,15 @@ package com.epam.conditions;
 public class BitwiseValuesSwap {
 
     public void swap(int first, int second) {
+        first = first ^ second;
+        second = first ^ second;
+        first = first ^ second;
+        System.out.println(first + " " + second);
+    }
+
+    public static void main(String[] args) {
+        BitwiseValuesSwap bvs = new BitwiseValuesSwap();
+        bvs.swap(7, 8);
     }
 
 }

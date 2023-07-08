88840275a40ab044b8635d814837087944f45f0e
Diyas Nurullayev <90145584+PrimeraAizen@users.noreply.github.com>
2023-07-08 23:24:08 +0600

    Solutions

--- src/main/java/com/epam/algorithms/ArrayTasks.java
+++ src/main/java/com/epam/algorithms/ArrayTasks.java
@@ -124,6 +124,7 @@ public class ArrayTasks {
                 }
             }
         }
+
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length ; j++) {
                 if(arr[i].length > arr[j].length){

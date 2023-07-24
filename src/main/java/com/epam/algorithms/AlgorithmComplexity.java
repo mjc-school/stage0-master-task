a49a2c2aaae5fb86583eb8a2cf46f2f497d59df1
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-07-24 16:29:28 +0400

    final

--- src/main/java/com/epam/algorithms/AlgorithmComplexity.java
+++ src/main/java/com/epam/algorithms/AlgorithmComplexity.java
@@ -14,29 +14,29 @@ public class AlgorithmComplexity {
     /**
      * Return the linear search complexity if the element you are looking for is at the end of the array.
      */
-    public Complexity badLinerSearch() {
-        return null;
-    }
-
-    /**
-     * Return the complexity of accessing an array element by index.
-     */
-    public Complexity arrayIndexItemAccess() {
-        return null;
-    }
-
-    /**
-     * Return the complexity of a binary search.
-     */
-    public Complexity binarySorting() {
-        return null;
-    }
-
-    /**
-     * Return the complexity of the twoCycleSorting algorithm.
-     */
-    public Complexity twoCycleSorting() {
-        return null;
-    }
+//    public Complexity badLinerSearch() {
+//        return null;
+//    }
+//
+//    /**
+//     * Return the complexity of accessing an array element by index.
+//     */
+//    public Complexity arrayIndexItemAccess() {
+//        return null;
+//    }
+//
+//    /**
+//     * Return the complexity of a binary search.
+//     */
+//    public Complexity binarySorting() {
+//        return null;
+//    }
+//
+//    /**
+//     * Return the complexity of the twoCycleSorting algorithm.
+//     */
+//    public Complexity twoCycleSorting() {
+//        return null;
+//    }
 
 }
--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -1,20 +1,13 @@
 package com.epam.loops;
 
-import java.util.Scanner;
 
 
 public class MultiplicationTable {
 
     public void printTable(int numberTableToPrint) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("enter integer");
-
-        int num = scanner.nextInt();
-
         for (int i = 0; i <= 9;i++) {
-            System.out.println(i+ " * " + numberTableToPrint + " = " + num * i);
+            System.out.println(i+ " * " + numberTableToPrint + " = " + numberTableToPrint * i);
         }
     }
 

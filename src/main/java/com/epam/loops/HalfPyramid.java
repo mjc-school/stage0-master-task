414bcb140f8e7b4050f42bb35ead33efed99ede4
Vitali Kalaskou <kolosokvit@gmail.com>
2023-07-31 23:11:21 +0300

    Complete Loops tasks

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,7 +3,16 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
-
+        if (cathetusLength > 0) {
+            StringBuilder floor = new StringBuilder();
+            while (floor.length() < cathetusLength) {
+                floor.append(" ");
+            }
+            for (int i = 1; i <= cathetusLength; i++) {
+                floor.append("*").delete(0,1);
+                System.out.println(floor);
+            }
+        }
     }
 
     public static void main(String[] args) {

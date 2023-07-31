5d4f6a29023d065bb321950702ca9a98c1c51bff
Vitali Kalaskou <kolosokvit@gmail.com>
2023-07-31 23:07:33 +0300

    Complete Conditions tasks

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -3,7 +3,20 @@ package com.epam.conditions;
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
-
+        if (year < 1 || month < 1 || month > 12) {
+            System.out.println("invalid date");
+        } else {
+            boolean yearIsLeap = year % 4 == 0 && year  % 100 != 0 || year % 400 == 0;
+            if (yearIsLeap && month == 2) {
+                System.out.println(29);
+            } else {
+                switch (month) {
+                    case 1, 3, 5, 7, 8, 11, 12 -> System.out.println(31);
+                    case 2 -> System.out.println(28);
+                    default -> System.out.println(30);
+                }
+            }
+        }
     }
 
 }

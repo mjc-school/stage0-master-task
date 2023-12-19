c828c33aab7be90a02230a0a53445e9595393e0e
Kirill Vlasovets <k.vlasovets@a1qa.com>
2023-12-20 03:42:17 +0400

    feature(OOP): task 1

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -2,4 +2,20 @@ package com.epam.OOP;
 
 public class Animal {
 
+    private final String color;
+    private final int numberOfPaws;
+    private final boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        return String.format("This animal is mostly %s. It has %s paws and %s fur.",
+                color,
+                numberOfPaws == 1 ? String.format("%d paw", numberOfPaws) : String.format("%d paws", numberOfPaws),
+                hasFur ? "a" : "no");
+    }
 }

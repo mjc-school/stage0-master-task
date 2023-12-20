e1d994a7bc76511bb37005f2dd8403073eac83dd
Kirill Vlasovets <k.vlasovets@a1qa.com>
2023-12-20 04:06:29 +0400

    feature(Lang Syntax): task 1

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,13 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int revertNumber = 0;
+        while (number != 0) {
+            revertNumber *= 10;
+            revertNumber += number % 10;
+            number /= 10;
+        }
+        System.out.println(revertNumber);
     }
 
 }

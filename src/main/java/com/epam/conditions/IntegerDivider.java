c67203503124d4767820ec1979926f3de2ce75d6
Kirill Vlasovets <k.vlasovets@a1qa.com>
2023-12-20 15:32:57 +0400

    feature(Conditions): task 4

--- src/main/java/com/epam/conditions/IntegerDivider.java
+++ src/main/java/com/epam/conditions/IntegerDivider.java
@@ -3,6 +3,15 @@ package com.epam.conditions;
 public class IntegerDivider {
 
     public void printCompletelyDivided(int dividend, int divider) {
+        try {
+            if (dividend / divider * divider == dividend) {
+                System.out.println("can be divided completely");
+            } else {
+                System.out.println("cannot be divided completely");
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("division by zero");
+        }
     }
 
 }

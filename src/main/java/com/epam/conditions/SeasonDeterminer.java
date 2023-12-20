85325b81ab52c458239b4a520bd9ad0c0dfb0ca0
Kirill Vlasovets <k.vlasovets@a1qa.com>
2023-12-20 04:16:26 +0400

    feature(Conditions): task 1

--- src/main/java/com/epam/conditions/SeasonDeterminer.java
+++ src/main/java/com/epam/conditions/SeasonDeterminer.java
@@ -3,6 +3,13 @@ package com.epam.conditions;
 public class SeasonDeterminer {
 
     public void tellTheSeason(int monthNumber) {
+        switch (monthNumber) {
+            case 12, 1, 2 -> System.out.println("winter");
+            case 3, 4, 5 -> System.out.println("spring");
+            case 6, 7, 8 -> System.out.println("summer");
+            case 9, 10, 11 -> System.out.println("autumn");
+            default -> System.out.println("Wrong month number");
+        }
     }
 
 }

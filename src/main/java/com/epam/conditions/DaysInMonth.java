39c430e00e9cc4d1c9bed5c658bfdb8491fbec58
Kirill Vlasovets <k.vlasovets@a1qa.com>
2023-12-20 15:26:36 +0400

    feature(Conditions): task 3

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -1,9 +1,11 @@
 package com.epam.conditions;
 
+import java.time.YearMonth;
+
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
-
+        System.out.println(YearMonth.of(year, month).lengthOfMonth());
     }
 
 }

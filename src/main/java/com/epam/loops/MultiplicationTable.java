b7d36dc12fcc067c330d5a10708ee7b08200fc05
Kirill Vlasovets <k.vlasovets@a1qa.com>
2023-12-21 14:51:10 +0400

    feature(Loops): task 1

--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -1,8 +1,16 @@
 package com.epam.loops;
 
+import java.util.Scanner;
+
 public class MultiplicationTable {
 
     public void printTable(int numberTableToPrint) {
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int number = scanner.nextInt();
 
+        for (int i = 1; i <= numberTableToPrint; i++) {
+            System.out.printf("%d x %d = %d%n", number, i, number * i);
+        }
+    }
 }

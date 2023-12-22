1f27b1b74a473a17c40063adde20520eddb87afd
Kirill Vlasovets <k.vlasovets@a1qa.com>
2023-12-22 15:08:34 +0400

    feature(Loops): task 2

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,7 +3,21 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
-
+        int counter = 1;
+        while (counter <= cathetusLength) {
+            for (int i = cathetusLength; i > 0; i--) {
+                if (i > counter) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(i);
+                }
+            }
+            for (int i = 2; i <= counter; i++) {
+                System.out.print(i);
+            }
+            System.out.println(" ");
+            counter++;
+        }
     }
 
     public static void main(String[] args) {

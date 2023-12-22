58ed5835773eea801230026a171bc8eb36dde2ef
Kirill Vlasovets <k.vlasovets@a1qa.com>
2023-12-22 15:15:27 +0400

    feature(Loops): task 4

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,7 +3,18 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
-
+        int counter = 1;
+        while (counter <= cathetusLength) {
+            for (int i = cathetusLength; i > 0; i--) {
+                if (i > counter) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println(" ");
+            counter++;
+        }
     }
 
     public static void main(String[] args) {
